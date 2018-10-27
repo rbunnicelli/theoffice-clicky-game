21b26847173bbdcd9ae4188f5c4707e584dbccae
Machoman <rbunnicelli2010@yahoo.com>
2018-10-27 10:49:31 -0400

    Updates

---
+++ precache-manifest.94844de3a02d3e505ae32c06052f8ac1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08e8adeae65e7e0b343e",
+    "url": "/theoffice-clicky-game/static/js/runtime~main.08e8adea.js"
+  },
+  {
+    "revision": "69da1e32d6ad113209e9",
+    "url": "/theoffice-clicky-game/static/js/main.69da1e32.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/theoffice-clicky-game/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "69da1e32d6ad113209e9",
+    "url": "/theoffice-clicky-game/static/css/main.55ad39ed.chunk.css"
+  },
+  {
+    "revision": "cc0dd9561b7040d69cadfe1406bf0331",
+    "url": "/theoffice-clicky-game/index.html"
+  }
+];
